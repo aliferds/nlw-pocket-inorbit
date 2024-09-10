35dd3d9a93a00d16ad35475c9ed9b1667826347f
Alifer Duarte Silva <aliferduartesilva@gmail.com>
2024-09-10 10:29:42 -0300

    Aula 02: routes and getWeekySummary

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,60 +1,34 @@
-import fastify from 'fastify'
-import { createGoal } from '../functions/create-goal'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import fastify, {
+  FastifyBaseLogger,
+  FastifyInstance,
+  FastifyPluginOptions,
+  FastifyTypeProvider,
+  RawServerDefault,
+} from 'fastify'
+import { createGoalsRoute } from './routes/createGoals'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import z from 'zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createPendingGoalsRoute } from './routes/pendingGoals'
+import { createCompletionsRoute } from './routes/createCompletions'
+import { getWeekSummaryRoute } from './routes/getWeekSummary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.get('/pending-goals', async () => {
-  const { pendingGoals } = await getWeekPendingGoals()
-
-  return { pendingGoals }
-})
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-    await createGoalCompletion({
-      goalId,
-    })
-  }
-)
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyfrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyfrequency } = request.body
-    await createGoal({
-      title,
-      desiredWeeklyfrequency,
-    })
-  }
-)
+app.register(createGoalsRoute)
+app.register(createPendingGoalsRoute)
+app.register(createCompletionsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
